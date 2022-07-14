3de6132199ec39d5e0115ee36412b7945023604c
addtobasic <addtobasic@gmail.com>
2022-07-14 21:41:53 +0900

    :sparkles: データ追加用Mutation: 終了

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
-import { useBooksQuery } from "./graphql/generated";
+import { useBooksQuery, useCreateBookMutation } from "./graphql/generated";
 
 function App() {
   const { data: { books = [] } = {} } = useBooksQuery();
+  const [createBook] = useCreateBookMutation({ refetchQueries: ["books"] });
+  const [title, setTitle] = useState("");
 
   return (
     <div>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <button
+        onClick={() => {
+          createBook({ variables: { params: { title: title } } });
+          setTitle("");
+        }}
+      >
+        保存
+      </button>
       {books.map((book) => (
         <div key={book.id}>{book.title}</div>
       ))}
